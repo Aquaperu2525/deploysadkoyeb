890fdfedb1eb40e096b1ebdbc1af96bb43652d9e
ingdantev <ingdantev@gmail.com>
2024-10-01 23:32:14 +0000

    actualizando del pakete

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'iniciando despliegue';
+  getHello(): string[] {
+    return ["iniciando despliegue","se cambio el puerto de 5000 a 8000","actualizando el package.json"];
   }
 }
