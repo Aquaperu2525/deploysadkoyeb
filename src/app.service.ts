8f636e09b4e82c2c6e52ad2b6548b2ab1676afe7
ingdantev <ingdantev@gmail.com>
2024-10-02 04:48:41 +0000

    fix package

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'primer elemento sin desplazar';
+    return 'modificando el pakete';
   }
 }
