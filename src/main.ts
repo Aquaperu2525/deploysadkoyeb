7efeeb574876c40438f762965252bba49285dd12
ingdantev <ingdantev@gmail.com>
2024-10-02 05:53:53 +0000

    al prncipio

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,11 @@
-import { ValidationPipe } from '@nestjs/common';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
-import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
+console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:true});
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist:true,
-      forbidNonWhitelisted:true,
-      transform:true
-    })
-  )
-  const writelist = [
-    "https://localhost:4200",
-    "http://localhost:4200",
-    "http://localhost:4200/*",
-   // "https://192.168.1.86:4444/*",
-    "https://192.168.1.86:4444",
-    "*"
-  ]
-  const corsOptions: CorsOptions = {
-    origin: function(origin,callback){
-      if(writelist.indexOf(origin) !== -1 || !origin ){
-        callback(null,true);
-      }else{
-        callback(new Error("not allow by corsw"))
-      }
-    },
-    methods: '*',
-    credentials: true,
-    optionsSuccessStatus: 204,
-  };
-  app.enableCors(corsOptions);
-  
-  useContainer(app.select(AppModule), {fallbackOnErrors: true}); 
-  await app.listen(process.env.PORT || 3000,()=>{
-    console.log(`Launching NestJS app on port ${process.env.PORT}, URL: http://0.0.0.0:${process.env.PORT}`)
-  });
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
 }
 bootstrap();
