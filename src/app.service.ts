5380b73d081f64755f41eeee268ca854e469df80
ingdantev <ingdantev@gmail.com>
2024-10-02 05:47:53 +0000

    modificando el main y el nestcli

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'modificando el pakete';
+    return 'cambiando el main';
   }
 }
