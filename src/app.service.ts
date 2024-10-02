7e9a9a056f9a63714941be710c83a0226cda618e
ingdantev <ingdantev@gmail.com>
2024-10-02 06:02:43 +0000

    modificando main

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'al principio';
+    return 'al principio, modificando el main';
   }
 }
