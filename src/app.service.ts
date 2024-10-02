72c77fcea6d3e5e6ce0b20c32668114dd70cbc7d
ingdantev <ingdantev@gmail.com>
2024-10-02 04:08:19 +0000

    re deploy

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string[] {
-    return ["iniciando despliegue","se cambio el puerto de 5000 a 8000","actualizando el package.json","retornando"];
-  }
-}
